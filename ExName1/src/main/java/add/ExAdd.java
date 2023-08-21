c724451d393196bfbc3d21203011472d8f23780e
muhammedshameer <muhammedshameer@localhost>
2023-08-21 11:29:44 +0530

    program has enn found

---
+++ ExName1/src/main/java/add/ExAdd.java
@@ -0,0 +1,24 @@
+package add;
+
+public class ExAdd {
+
+	public int add()
+	{
+		int a=10;
+		int b=10;
+		int c=a+b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExAdd s=new ExAdd();
+		int x=s.add();
+		System.out.print("The Sum Is "+x);
+
+	}
+
+
+	}
+
+
